f487e8e06a844935808557b275daec9d0c11f702
curtis <chiegithub@gmail.com>
2021-01-15 13:44:50 +0300

    Update katocoin.sol

--- katocoin.sol
+++ katocoin.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.4.16;
 
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extradata)external;}
